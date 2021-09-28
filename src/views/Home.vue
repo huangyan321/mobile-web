8ed8503c712508f2ea21bf9e0463748d57ffcadc
huangyan321 <1604549268@qq.com>
2021-09-29 00:28:07 +0800

    添加top-bar样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="nav-top">
+      <div class="bgc-black-light p-y-3 d-fl jc-around" style="height: 4rem">
+        <div class="d-fl ai-center p-l-4 flex-1">
+          <div class="sprite sprite-logo"></div>
+          <div class="m-l-3">
+            <div class="fs-lg color-white">王者荣耀</div>
+            <div class="color-grey fs-xs"><span>团队成就更多</span></div>
+          </div>
+        </div>
+        <button class=" bgc-primary btn m-x-3">立即下载</button>
+      </div>
+    </div>
+    <div class="main">内容</div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
+<style lang="scss" >
+
+</style>>
+
