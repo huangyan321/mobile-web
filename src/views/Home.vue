00411347d5aa5af80b42ebe33190b0fabbea8d5f
huangyan321 <1604549268@qq.com>
2021-09-29 14:59:10 +0800

    宣传横幅样式完善

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,10 +9,43 @@
             <div class="color-grey fs-xs"><span>团队成就更多</span></div>
           </div>
         </div>
-        <button class=" bgc-primary btn m-x-3">立即下载</button>
+        <button class="bgc-primary btn m-x-3">立即下载</button>
       </div>
     </div>
-    <div class="main">内容</div>
+    <div class="main bgc-primary" style="height: 3rem">
+      <div class="nav">
+        <ul class="d-fl ai-center jc-around" style="height: 100%">
+          <li class="nav-item">
+            <router-link
+              class="nav-link d-fl jc-center ai-center color-white"
+              to="/main"
+              ><span class="active active-item">首页</span></router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link
+              class="nav-link d-fl jc-center ai-center color-white"
+              to="/main"
+              ><span>攻略中心</span></router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link
+              class="nav-link d-fl jc-center ai-center color-white"
+              to="/main"
+              ><span>赛事中心</span></router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link
+              class="nav-link d-fl jc-center ai-center color-white"
+              to="/main"
+              ><span>IP新游</span></router-link
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
     <router-view></router-view>
   </div>
 </template>
@@ -26,6 +59,18 @@ export default {
 };
 </script>
 <style lang="scss" >
+.nav {
+  height: 100%;
+}
 
+.nav-item {
+  height: 100%;
+  width: 25%;
+  flex: 1;
+}
+.nav-link {
+  width: 100%;
+  height: 100%;
+}
 </style>>
 
