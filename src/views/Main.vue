546c07072f0cf15a5ee4e7ea4cefb67db1be4637
huangyan321 <1604549268@qq.com>
2021-09-29 14:59:28 +0800

    抽离组件

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,15 +1,29 @@
 <template>
-  <div>
-    主页
+  <div class="main">
+    <div class="swiper">
+      <Swiper></Swiper>
+    </div>
+    <div>
+      <Navigate></Navigate>
+    </div>
+    <div>
+      <News></News>
+    </div>
   </div>
 </template>
 
 <script>
+import Swiper from "../components/main/Swiper";
+import Navigate from "../components/main/Navigate";
+import News from "../components/main/News";
 export default {
-
-}
+  components: {
+    Swiper,
+    Navigate,
+    News,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style lang="scss" scoped>
+</style>>
