2b67f2a2e40977698d0a3b316a29a2b6d9a87ffb
huangyan321 <1604549268@qq.com>
2021-09-30 11:53:13 +0800

    入口列表增加展开收起功能

--- src/components/main/Navigate.vue
+++ src/components/main/Navigate.vue
@@ -1,7 +1,47 @@
 <template>
   <div class="nav">
-    <div class="entry-list">
+    <div class="entry-list" v-if="isOpen">
       <ul class="d-fl flex-wrap bgc-white m-t-3 color-dark">
+        <li class="p-y-3 d-fl flex-c ai-center">
+          <i class="d-bl sprite sprite-baoliao"></i><span>爆料站</span>
+        </li>
+        <li class="p-y-3 d-fl flex-c ai-center">
+          <i class="d-bl sprite sprite-baoliao"></i><span>爆料站</span>
+        </li>
+        <li class="p-y-3 d-fl flex-c ai-center">
+          <i class="d-bl sprite sprite-baoliao"></i><span>爆料站</span>
+        </li>
+        <li class="p-y-3 d-fl flex-c ai-center">
+          <i class="d-bl sprite sprite-baoliao"></i><span>爆料站</span>
+        </li>
+        <li class="p-y-3 d-fl flex-c ai-center">
+          <i class="d-bl sprite sprite-baoliao"></i><span>爆料站</span>
+        </li>
+        <li class="p-y-3 d-fl flex-c ai-center">
+          <i class="d-bl sprite sprite-baoliao"></i><span>爆料站</span>
+        </li>
+        <li class="p-y-3 d-fl flex-c ai-center">
+          <i class="d-bl sprite sprite-baoliao"></i><span>爆料站</span>
+        </li>
+        <li class="p-y-3 d-fl flex-c ai-center">
+          <i class="d-bl sprite sprite-baoliao"></i><span>爆料站</span>
+        </li>
+        <li class="p-y-3 d-fl flex-c ai-center">
+          <i class="d-bl sprite sprite-baoliao"></i><span>爆料站</span>
+        </li>
+        <li class="p-y-3 d-fl flex-c ai-center">
+          <i class="d-bl sprite sprite-baoliao"></i><span>爆料站</span>
+        </li>
+        <li class="p-y-3 d-fl flex-c ai-center">
+          <i class="d-bl sprite sprite-baoliao"></i><span>爆料站</span>
+        </li>
+        <li class="p-y-3 d-fl flex-c ai-center">
+          <i class="d-bl sprite sprite-baoliao"></i><span>爆料站</span>
+        </li>
+      </ul>
+    </div>
+    <div class="entry-list" style="width: 100%; overflow-x: auto" v-else>
+      <ul class="d-in-fl bgc-white m-t-3 color-dark">
         <li class="p-y-3 d-fl flex-c ai-center">
           <i class="d-bl sprite sprite-baoliao"></i><span>爆料站</span>
         </li>
@@ -43,23 +83,36 @@
     <div
       class="entry-button p-y-2 bgc-light text-center"
       style="height: 2.1rem"
+      @click="entry"
     >
-      <span class="sprite sprite-shouqi"></span><span>收起</span>
+      <span
+        :class="[
+          'sprite',
+          { ' sprite-shouqi': isOpen, 'sprite-open': !isOpen },
+        ]"
+      ></span
+      ><span>{{ isOpen ? "收起" : "展开" }}</span>
     </div>
   </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      isOpen: true,
+    };
   },
   mounted() {},
-  methods: {},
+  methods: {
+    entry() {
+      this.isOpen = !this.isOpen;
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
 .nav li {
-  width: 25%;
+  width: 7.2115rem;
   position: relative;
 }
 .nav li::after {
